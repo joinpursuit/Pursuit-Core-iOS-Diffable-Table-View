c997720d6631ee540676ae2aa800034452099617
Ben Stone <benjamin.h.stone@gmail.com>
2020-01-29 11:27:55 -0500

    Add BundleFetchingService + tests

--- TableViewsWithDiffableDataSource/Episode.swift
+++ TableViewsWithDiffableDataSource/Episode.swift
@@ -4,13 +4,5 @@ struct Episode: Codable {
     let name: String
     let runtime: Int
     let summary: String
-    
-    static func getEpisodes(from data: Data) -> [Episode] {
-        guard let episodes = try? JSONDecoder().decode([Episode].self, from: data) else {
-            print("Error serializing data")
-            return []
-        }
-        return episodes
-    }
 }
 
